0dc15e372af45c05cda03605c7eb234953566c5b
Dmitry Budyakov <dmitry.budyakov@gmail.com>
2025-02-27 11:00:07 +0300

    Refactor

--- maximum_students_on_a_single_bench/solution.py
+++ maximum_students_on_a_single_bench/solution.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from typing import List
 
 
-def solution(students: List[List[int]]) -> int:
+def max_students(students: List[List[int]]) -> int:
     benches = defaultdict(set)
     for student_id, bench_id in students:
         benches[bench_id].add(student_id)
--- maximum_students_on_a_single_bench/tests.py
+++ maximum_students_on_a_single_bench/tests.py
@@ -1,37 +1,37 @@
 import unittest
 
-from solution import solution
+from maximum_students_on_a_single_bench.solution import max_students
 
 
 class TestSolution(unittest.TestCase):
 
     def test_zero(self):
-        real = solution([])
+        real = max_students([])
         expected = 0
         self.assertEqual(real, expected)
 
     def test_one(self):
-        real = solution([[1,2], [2,2], [3,3], [1,3], [2,3]])
+        real = max_students([[1,2], [2,2], [3,3], [1,3], [2,3]])
         expected = 3
         self.assertEqual(real, expected)
 
     def test_two(self):
-        real = solution([[1,1], [2,1], [3,1], [4,2], [5,2]])
+        real = max_students([[1,1], [2,1], [3,1], [4,2], [5,2]])
         expected = 3
         self.assertEqual(real, expected)
 
     def test_three(self):
-        real = solution([[1,1], [1,1]])
+        real = max_students([[1,1], [1,1]])
         expected = 1
         self.assertEqual(real, expected)
 
     def test_four(self):
-        real = solution([[1,1], [1,2], [2,1], [3,3], [1,1], [2,3], [4,3]])
+        real = max_students([[1,1], [1,2], [2,1], [3,3], [1,1], [2,3], [4,3]])
         expected = 3
         self.assertEqual(real, expected)
 
     def test_five(self):
-        real = solution([[1,1], [1,2], [2,1], [2,2]])
+        real = max_students([[1,1], [1,2], [2,1], [2,2]])
         expected = 2
         self.assertEqual(real, expected)
 
